728103dbd5e0e3408d7abab649b115e6d6e6fa5d
phlebotomist <206363060+phlebotomist@users.noreply.github.com>
2025-12-15 12:42:34 -0500

    base plugin info

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using VampireCommandFramework;
+
+namespace DeadlyDetails;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("gg.deca.VampireCommandFramework")]
+// [BepInDependency("gg.deca.Bloodstone")]
+// [Bloodstone.API.Reloadable]
+public class Plugin : BasePlugin
+{
+    Harmony _harmony;
+
+    public static ManualLogSource L;
+
+    public override void Load()
+    {
+        L = Log;
+        L.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} loaded!");
+
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+        CommandRegistry.RegisterAll();
+
+    }
+
+    public override bool Unload()
+    {
+        CommandRegistry.UnregisterAssembly();
+        _harmony?.UnpatchSelf();
+        return true;
+    }
+}
